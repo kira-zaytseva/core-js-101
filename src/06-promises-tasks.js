14d38ea03bf6f5f21e166366994ba590a8141188
Kira Zaytseva <kira.zaytseva97@gmail.com>
2023-01-03 02:55:48 +0300

    feat: solve some tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -53,8 +53,8 @@ function willYouMarryMe(isPositiveAnswer) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -76,8 +76,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
